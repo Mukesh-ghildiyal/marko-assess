12fa97ad05388e47f74b71856030c2f29a48f319
Mukesh-Ghildiyal <mukeshghildiyal435@gmail.com>
2025-12-25 02:40:33 +0530

    add frontend url

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || '*',
+    origin: 'https://resilient-elf-fbe5b8.netlify.app/'|| '*',
     credentials: true
 }));
 
